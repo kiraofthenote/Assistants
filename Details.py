f546a9f51097a66a167a8219789b7c8ef991486a
kiraofthenote <96733021+kiraofthenote@users.noreply.github.com>
2021-12-30 22:56:48 +0530

    Add files via upload

---
+++ Details.py
@@ -0,0 +1,20 @@
+# my_kira_email = 'youremail'
+# my_kira_password = 'yourpassword'
+# Records = {
+#            'Person1' : 'emailofPerson1',   
+#            'Person2' : 'emailofPerson2'
+# }
+months = {
+           1 : 'Jan',
+           2 : 'Feb',
+           3 : 'Mar',
+           4 : 'Apr',
+           5 : 'May',
+           6 : 'June',
+           7 : 'July',
+           8 : 'August',
+           9 : 'September',
+           10 : 'October',
+           11 : 'November',
+           12 : 'December'
+}
