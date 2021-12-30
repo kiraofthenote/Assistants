9c0442c0c98b7d6805eebe3f29408a675ee5be97
kiraofthenote <96733021+kiraofthenote@users.noreply.github.com>
2021-12-30 20:45:43 +0530

    Add files via upload

---
+++ Details.py
@@ -0,0 +1,22 @@
+my_email = 'avkhalkar@gmail.com'
+password = 'aditya_237'
+# my_kira_email = your email
+# my_kira_password = your password
+Records = {
+            'mother' : 'jvkhalkar@git-india.edu.in',
+            'father' : 'vikasrkhalkar@gmail.com'
+        }
+months = {
+           1 : 'Jan',
+           2 : 'Feb',
+           3 : 'Mar',
+           4 : 'Apr',
+           5 : 'May',
+           6 : 'June',
+           7 : 'July',
+           8 : 'August',
+           9 : 'September',
+           10 : 'October',
+           11 : 'November',
+           12 : 'December'
+}
